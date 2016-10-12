103f59342f7346714378e98070969e5216f8d3c3
Bartek <bartlomiej.stapor@gmail.com>
2016-10-13 01:05:18 +0200

    Added working 2.10

---
+++ Zadania/2.10.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-2 -*-
+
+line = "Lorem ipsum dolor sit amet\nconsectetur adipiscing elit.\nEtiam blandit luctus consectetur."
+print line;
+lista = line.split();
+print "Ilosc wyrazow: ", len(lista);
