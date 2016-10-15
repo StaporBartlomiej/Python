19e10533b3f1a9cee5f2b88c784595ca13c210a7
Bartek <bartlomiej.stapor@gmail.com>
2016-10-15 18:59:59 +0200

    Added second way of doing this

--- Zadania/2.15.py
+++ Zadania/2.15.py
@@ -5,3 +5,10 @@ L = [1,2,3,4,5]
 S = ""
 Result = S.join(str(x) for x in L) # str() - Return a string version of object
 print (Result)
+
+#or simply
+
+B = ['1','2','3','4','5']
+D = ""
+Result2 = D.join(B)
+print("\nSecond way\n",Result2)
