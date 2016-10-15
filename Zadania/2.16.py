43b1c8394d00749208b06080a6264902b97993ce
Bartek <bartlomiej.stapor@gmail.com>
2016-10-15 19:16:15 +0200

    Added working 2.16

---
+++ Zadania/2.16.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-2 -*-
+
+line = "Lorem ipsum dolor sit GvR amet\nconsectetur adipiscing elit.\nEtiam blandit luctus consectetur.";
+print (line.replace("GvR","Guido van Rossum"))
+    
+
+
